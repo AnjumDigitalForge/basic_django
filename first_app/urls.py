e37cb7b0e217ce80992ac72eb99795c658cab5eb
AnjumDigitalForge <mnaveedanjum62@gmail.com>
2026-02-23 00:22:37 +0500

    learning & developing django from scratch

---
+++ first_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views #(to call the views.py file from the same directory)
+urlpatterns = [
+#	path('', views.home) # ‘’ it means, if path is empty so go to “def home(request):” of views.py
+	path('myname/', views.myname),
+	path('yourname/', views.yourname),
+]
---
+++ first_app/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def myname(request):
+    return HttpResponse('Hello Me!')
+#    return render(request, 'first_app/mypage.html', {'name': 'Naveed'})
+
+def yourname(request):
+    return HttpResponse('Hello You!')
