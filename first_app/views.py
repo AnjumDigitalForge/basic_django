4f971c973a70286baff70875fb27db64f2491d10
AnjumDigitalForge <mnaveedanjum62@gmail.com>
2026-02-24 02:47:48 +0500

    creating modules

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from first_app.models import BlogPost
+
 # Create your views here.
 def myname(request):
     return HttpResponse('Hello Me!')
@@ -8,3 +10,7 @@ def myname(request):
 
 def yourname(request):
     return HttpResponse('Hello You!')
+
+def test_models(request):
+    count = BlogPost.objects.count()
+    return HttpResponse(f"Number of blog posts in database: {count}")
