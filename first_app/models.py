9b312b3243c5ec468b9df191a7d23e54bca72a7d
AnjumDigitalForge <mnaveedanjum62@gmail.com>
2026-02-26 21:44:15 +0500

    making everything up to date

--- first_app/models.py
+++ first_app/models.py
@@ -7,9 +7,15 @@ class std_sign_up(models.Model):
     student_name = models.CharField(max_length=100)
     father_name = models.CharField(max_length=100)
     age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    grade = models.CharField(max_length=10)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=10)
-    auth_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    auth_user = models.OneToOneField(User, on_delete=models.CASCADE) #Relationship field
     
-#    def __str__(self):
-#        return self.student_name
+    def __str__(self): #This method is used to return a string representation of the object. It is called when you print an instance of the model or when you display it in the Django admin interface.
+        return self.student_name
+    
+    class Meta:
+        verbose_name = 'Student Sign Up'        # Singular name in replacement of 'std_sign_up' the class / model name
+        verbose_name_plural = 'Student Sign Ups' # Plural name in replacement of 'std_sign_up' the class / model name
