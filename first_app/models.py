d9a97bc050ad4ff215759a4fddda9095b157d6bd
AnjumDigitalForge <mnaveedanjum62@gmail.com>
2026-03-02 16:02:27 +0500

    creation of model 'TeacherSignUp'

--- first_app/models.py
+++ first_app/models.py
@@ -15,4 +15,16 @@ class StdSignUp(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.std_name}"
-    
+
+class TeacherSignUp(models.Model):
+    teacher_name = models.CharField(max_length=100)
+    teacher_father_name = models.CharField(max_length=100)
+    teacher_age = models.IntegerField()
+    teacher_qualification = models.CharField(max_length=50)
+    teacher_contact = models.CharField(max_length=15)
+    teacher_email = models.EmailField()
+    teacher_password = models.CharField(max_length=20)
+    user = models.ManyToManyField(User, blank=True)  # Establishes a many-to-many relationship with the User model
+
+    def __str__(self):
+        return f"{self.id} - {self.teacher_name}"
